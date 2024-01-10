472c095d0d1bc129cb74bb051175b0974ec8ab75
minsesil <148636941+minsesil@users.noreply.github.com>
2024-01-11 07:28:11 +0900

    Create MemberEnrollFormController.java

---
+++ mybatis/src/main/java/com/study/mybatis/member/controller/MemberEnrollFormController.java
@@ -0,0 +1,21 @@
+package com.study.mybatis.member.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class MemberEnrollFormController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("WEB-INF/views/member/memberEnrollForm.jsp").forward(request, response);
+	}
+
+}

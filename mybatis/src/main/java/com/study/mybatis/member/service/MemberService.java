267a6e79a5ad328e71c9ff64ec24208938a90b0c
minsesil <148636941+minsesil@users.noreply.github.com>
2024-01-11 07:28:28 +0900

    Create MemberService.java

---
+++ mybatis/src/main/java/com/study/mybatis/member/service/MemberService.java
@@ -0,0 +1,17 @@
+package com.study.mybatis.member.service;
+
+import com.study.mybatis.member.vo.Member;
+
+public interface MemberService {
+	//인터페이스 : 상수필드(public static final) + 추상메소드(public abstract)
+	
+	/* public abstract */ Member loginMember(Member m);
+	
+	int insertMember(Member m);	
+	
+	int checkId(String userId);
+	
+	int updateMember(Member m);
+	
+	int deleteMember(String userId);
+}

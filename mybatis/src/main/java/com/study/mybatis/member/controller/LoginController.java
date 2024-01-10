6292c3b62e68c9d331186f6ae8ac4c3f6794c573
minsesil <148636941+minsesil@users.noreply.github.com>
2024-01-11 07:28:03 +0900

    Create LoginController.java

---
+++ mybatis/src/main/java/com/study/mybatis/member/controller/LoginController.java
@@ -0,0 +1,38 @@
+package com.study.mybatis.member.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import com.study.mybatis.member.service.MemberServiceImpl;
+import com.study.mybatis.member.vo.Member;
+
+
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Member m = new Member();
+		m.setUser_id(request.getParameter("userId"));
+		m.setUser_pwd(request.getParameter("userPwd"));
+		
+		Member loginUSer = new MemberServiceImpl().loginMember(m);
+		
+		if(loginUSer==null) {  //로그인 실패
+			request.setAttribute("errorMsg", "로그인 실패");
+			request.getRequestDispatcher("WEB-INF/views/common/errorPage.jsp").forward(request, response);
+			
+		}else {
+			request.getSession().setAttribute("loginUser", loginUSer);
+			response.sendRedirect(request.getContextPath());
+			//getContextPath() : root path
+			
+		}
+	}
+
+}

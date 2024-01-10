a76a69e49ff05cab7bfde9862b2730b37e21b257
minsesil <148636941+minsesil@users.noreply.github.com>
2024-01-11 07:26:39 +0900

    Create BoardInsertFormController.java

---
+++ mybatis/src/main/java/com/study/mybatis/board/controller/BoardInsertFormController.java
@@ -0,0 +1,22 @@
+package com.study.mybatis.board.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class BoardInsertFormController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("WEB-INF/views/board/boardInsertForm.jsp").forward(request, response);
+	
+	}
+
+}

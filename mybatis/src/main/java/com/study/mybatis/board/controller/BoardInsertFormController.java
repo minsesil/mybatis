c2aa2bba9f889c386df4fb0112583d6513048048
minsesil <148636941+minsesil@users.noreply.github.com>
2024-01-11 17:58:21 +0900

    Update BoardInsertFormController.java

--- mybatis/src/main/java/com/study/mybatis/board/controller/BoardInsertFormController.java
+++ mybatis/src/main/java/com/study/mybatis/board/controller/BoardInsertFormController.java
@@ -12,7 +12,7 @@ public class BoardInsertFormController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		request.getRequestDispatcher("WEB-INF/views/board/boardInsertForm.jsp").forward(request, response);
